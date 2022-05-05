21b64b66bb82dfc7e29fd392ad2182304e73a64d
ancs214 <ancs214@gmail.com>
2022-05-05 16:18:34 -0400

    added PUT routes

--- apiRoutes/index.js
+++ apiRoutes/index.js
@@ -22,12 +22,29 @@ router.get('/departments', (req, res) => {
 })
 
 
+router.post('/departments', ({ body }, res) => {
+    const sql = `INSERT INTO department (name) VALUES (?)`;
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+          }
+          res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    })
+})
+
 
 //    ROLES ROUTES
 
 
 router.get('/roles', (req, res) => {
-    const sql = `SELECT * FROM roles`;
+    const sql = `SELECT * FROM roles LEFT JOIN department on department_id = department.id;`;
      db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
@@ -38,6 +55,24 @@ router.get('/roles', (req, res) => {
             data: rows
         });
     });
+})
+
+
+router.post('/roles', ({ body }, res) => {
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+    const params = [body.title, body.salary, body.department_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+          }
+          res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    })
 })
 
 
@@ -58,5 +93,46 @@ router.get('/employees', (req, res) => {
         });
     });
 })
+
+router.post('/employees', ({ body }, res) => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+          }
+          res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    })
+})
+
+router.put('/employees/:id', (req, res) => {
+    const sql = `UPDATE employee SET first_name = ?, last_name = ?, role_id = ? WHERE id = ?`;
+    const params = [req.body.first_name, req.body.last_name, req.body.role_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+          } else if (!result.affectedRows) {
+            res.json({
+              message: 'Employee not found'
+            });
+          } else {
+            res.json({
+              message: 'success',
+              data: req.body,
+              changes: result.affectedRows
+            });
+          }
+    })
+})
+
+
+
 
 module.exports = router; 
